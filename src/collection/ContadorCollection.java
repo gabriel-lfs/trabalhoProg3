d3cb49c66acb17596e040eedbd07dddf563e964a
Gabriel Luís Fernando de Souza <gabriel-soz@live.com>
2018-06-29 18:41:06 -0300

    adcionada implementação do método collection

---
+++ src/collection/ContadorCollection.java
@@ -0,0 +1,41 @@
+package collection;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ContadorCollection {
+    public String contar(String entrada) {
+        String output = "[";
+        ArrayList<String> palavras = new ArrayList<>();
+        ArrayList<Integer> quantidades = new ArrayList<>();
+        String[] aux = entrada.toLowerCase().replaceAll("[\\d,.]", "").split(" ");
+        
+        Arrays.sort(aux);
+        ArrayList<String> sorted = new ArrayList<String>(Arrays.asList(aux));
+        
+        for (String s : sorted) {
+            if (!palavras.contains(s)) {
+                palavras.add(s);
+                quantidades.add(1);
+            } else {
+                Integer n = quantidades.remove(palavras.indexOf(s));
+                quantidades.add(palavras.indexOf(s), n+1);
+            }
+        }
+        for (int i = 0; i < palavras.size(); i++) {
+            output += palavras.get(i)+'-';
+            if (i != palavras.size()-1) {
+                output += String.valueOf(quantidades.get(i))+"; ";
+            } else {
+                output += String.valueOf(quantidades.get(i))+"]";
+            }
+        }
+        
+        return output;
+    }
+    
+    public static void main(String[] args) {
+        ContadorCollection c = new ContadorCollection();
+        
+        System.out.println(c.contar("This is a test, I hope this works propperly as I don't want to test it again."));
+    }
+}
--- src/mapa/ContadorCollection.java
+++
@@ -1,7 +0,0 @@
-package mapa;
-
-public class ContadorCollection {
-	public static void main(String[] args) {
-		
-	}
-}
